e49ac3f3edbbc0827b47d3e549f1f35044455428
Robert Schroll <robert@thedataincubator.com>
2024-09-25 22:28:46 +0000

    Task 6.4.5

--- app.py
+++ app.py
@@ -6,9 +6,14 @@ if torch.cuda.is_available():
 else:
     device = "cpu"
 
+LORA_WEIGHTS = "rschroll/maya_model_v1_lora"
+
 def load_model():
     pipeline = diffusers.AutoPipelineForText2Image.from_pretrained(
         "CompVis/stable-diffusion-v1-4", torch_dtype=torch.float16
+    )
+    pipeline.load_lora_weights(
+        LORA_WEIGHTS, weight_name="pytorch_lora_weights.safetensors"
     )
     pipeline.to(device)
     return pipeline
