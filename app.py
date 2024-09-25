bc1f83fb1e5d237d6b8af4fb6f0f71934d091412
Robert Schroll <robert@thedataincubator.com>
2024-09-25 22:18:02 +0000

    Task 6.4.4

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import diffusers
 import torch
 
 if torch.cuda.is_available():
     device = "cuda"
 else:
     device = "cpu"
+
+def load_model():
+    pipeline = diffusers.AutoPipelineForText2Image.from_pretrained(
+        "CompVis/stable-diffusion-v1-4", torch_dtype=torch.float16
+    )
+    pipeline.to(device)
+    return pipeline
